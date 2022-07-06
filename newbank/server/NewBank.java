8c4ce790339a9bc9fa1ea1c6da9904eb1011ba82
fkoh111 <frederik_kok@icloud.com>
2022-07-06 18:21:45 +0530

    Proposal to close addition-prompt-user-possibilities

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,37 +1,39 @@
 package newbank.server;
 
 import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
 
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
-	
+	private HashMap<String, Customer> customers;
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
 		customers.put("Bhagy", bhagy);
-		
+
 		Customer christina = new Customer();
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
+
 		Customer john = new Customer();
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		if (customers.containsKey(userName)) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -39,17 +41,47 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+		if (customers.containsKey(customer.getKey())) {
+			switch (request) {
+				case "SHOWMYACCOUNTS":
+					return showMyAccounts(customer);
+				case "HELP":
+					return showHelp();
+				default:
+					return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
 
+	private String showHelp() {
+		String help = "\nSHOWMYACCOUNTS\n"
+		+ "├ Returns a list of all the customers accounts along with their current balance\n"
+		+ "└ e.g. Main: 1000.0\n"
+
+		+ "\n"
+
+		+ "NEWACCOUNT <Name>\n"
+		+ "├ Creates a new account\n"
+		+ "└ e.g. NEWACCOUNT Savings\n"
+
+		+ "\n"
+
+		+ "MOVE <Amount> <From> <To>\n"
+		+ "├ Moves money between a users existing accounts\n"
+		+ "└ e.g. MOVE 100 Main Savings\n"
+
+		+ "\n"
+
+		+ "PAY <Person/Company> <Ammount>\n"
+		+ "├ Pay another user from your account to their account\n"
+		+ "└ e.g. PAY John 100\n";
+		return help;
+	
+	}
 }
+	
--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -33,7 +33,10 @@ public class NewBankClientHandler extends Thread{
 			CustomerID customer = bank.checkLogInDetails(userName, password);
 			// if the user is authenticated then get requests from the user and process them 
 			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
+				out.println("Log In Successful. What do you want to do?\n"
+				+ "\n"
+				+ "Type HELP to list available commands"
+				+ "\n");
 				while(true) {
 					String request = in.readLine();
 					System.out.println("Request from " + customer.getKey());
