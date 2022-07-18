f6f528ae42b9ec85baa2db8e717656e8182837d3
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-07-18 04:02:26 +0100

    Create a customers account

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -52,6 +52,30 @@ public class Customer {
 		return lastName;
 	}
 
+	/**
+	 * Takes a request, and creates a new account for a 
+	 * specified customer.
+	 *
+	 * @param customer the customer creating the new account
+	 * @param request  the command and arguments passed in through the command line
+	 * @return         the status of the transfer as a string
+	 */
+	public boolean createAccount(Customer customer, String accountName, double accountNumber) {
+		
+		try {
+			// Create the new account
+			Account newAccount = new Account.AccountBuilder(accountName, accountNumber, accountNumber).build();
+
+			// Add the account to the list of users accounts
+			this.addAccount(newAccount);
+
+			return true;
+
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	public Account getAccount(String name){
 		for(Account a : accounts) {
 			if (name.equals(a.getAccountName().toLowerCase())){
